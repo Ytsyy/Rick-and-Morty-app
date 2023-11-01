595524f9aebebb9edd543683cf9c19ec56f05674
student <student@MacBook-Pro-2.local>
2023-11-01 17:29:28 +0700

    SecondHomeworkDone

--- iosSchool_HH/FirstHomework.swift
+++ iosSchool_HH/FirstHomework.swift
@@ -64,7 +64,13 @@ class Character {
     }
 }
 
-class CharacterGenerator {
+protocol CharacterGeneratorProtocol {
+    var characterCount: Int { get }
+    func printCharacterInfo(character: Character)
+}
+
+class CharacterGenerator: CharacterGeneratorProtocol {
+    var characterCount: Int = 0
 
     func generateCharacter() -> Character {
         let randomID = Int.random(in: 0...1000)
@@ -78,6 +84,11 @@ class CharacterGenerator {
             gender: Character.Gender.allCases.randomElement() ?? .unknown,
             status: Character.Status.allCases.randomElement() ?? .unknown
         )
+        characterCount += 1
         return someCharacter
+    }
+
+    func printCharacterInfo(character: Character) {
+        print(character)
     }
 }
