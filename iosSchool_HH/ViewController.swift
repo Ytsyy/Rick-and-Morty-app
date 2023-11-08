89097cc0981634e8459da6a35141ea3d0010cd72
student <student@MacBook-Pro-2.local>
2023-11-08 18:28:36 +0700

    Fourth homework done

--- iosSchool_HH/ViewController.swift
+++ iosSchool_HH/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         let generator = ColorGenerator(alpha: 1)
-        let result1 = arrayInt.filter { $0 % 2 == 0}
         let array = [0.1, 0.2, 0.5, 1]
         let generators = array.map { ColorGenerator(alpha: $0) }
         let test = array.map {alpha -> ColorGeneratorProtocol? in
@@ -31,40 +30,53 @@ class ViewController: UIViewController {
         let test2 = test1.compactMap{ $0 }
         let test3 = test1.compactMap{ $0?.alpha }
         let test4 = test1.map { $0?.alpha }
-        //let color = Color(red: 1, green: 1, blue:1)
-        //let intArray = generator.convertToArray(element: 1)
-        //let colorArr = generator.convertToArray(element: color)
         let arrayInt = [1, 2, 3, 4, 5, 6, 7, 8]
         let resultReduce = arrayInt.reduce([0]) { partialResult, element in
             [(partialResult.first ?? 0) + element ]
         }
         let result = arrayInt.filter { elem in
             return elem % 2 == 0
-                                     }
-      
-        let characterGenerator: CharacterGeneratorProtocol = CharacterGenerator()
-        let characterGeneratorInstance = CharacterGenerator()
-        let generatedCharacter1 = characterGenerator.generateCharacter()
-        let generatedCharacter2 = characterGeneratorInstance.generateCharacter()
-
-        let nameThreeHandler = CharacterGenerator().generateNameThree()
-        let nameFourClosure = CharacterGenerator().generateNameFour()
-
-        CharacterGenerator().generateNameOne { name in
-            print("First generated name: \(name)")
+            let result1 = arrayInt.filter { $0 % 2 == 0}
         }
 
-        print("Second generated name: ", terminator: "")
-        CharacterGenerator().generateNameTwo {
-            String(Int.random(in: 0...1000))
+        let characters = (0..<10).map { _ in CharacterGenerator().generateCharacter()}
+        let maleCharacters = characters.filter { $0.gender == .male }
+
+        let charactersWithAInName = characters.filter { $0.name.contains("a") || $0.name.contains("A") }
+
+        let names = ["John", "Jim", "Jojo"]
+        let charactersFromNames = names.map { name in
+            let randomCharacter = CharacterGenerator().generateCharacter()
+            return Character(
+                id: randomCharacter.id,
+                name: name,
+                species: randomCharacter.species,
+                image: randomCharacter.image,
+                url: randomCharacter.url,
+                episode: randomCharacter.episode,
+                gender: randomCharacter.gender,
+                status: randomCharacter.status
+            )
         }
 
-        print("Third generated name: ", terminator: "")
-        nameThreeHandler()
+        let episodeNames = ["Episode 1", "Episode 2", "Episode 3"]
+        let charactersForEpisodesUsingMap = episodeNames.map { _ in
+            (1...3).map { _ in
+                CharacterGenerator().generateCharacter()
+            }
+        }
+        let charactersForEpisodesUsingCompactMap = episodeNames.compactMap { _ in
+            (1...3).map { _ in
+                CharacterGenerator().generateCharacter()
+            }
+        }
 
-        print("Fourth generated name: \(nameFourClosure())")
+        let totalCharacterNames = characters.reduce("") { result, character in
+            return result + character.name
+        }
 
         generator.changeColor {
-            [0, 100, 200] 
+            [0, 100, 200]
+        }
     }
 }
