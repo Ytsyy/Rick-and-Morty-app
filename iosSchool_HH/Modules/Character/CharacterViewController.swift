8d0faa279fafcf47d3d8708551fb7aab7252db20
Maxim <Maxim@MacBook-Air-test.local>
2024-03-21 14:22:15 +0700

    Fix back image in characters screen

--- iosSchool_HH/Modules/Character/CharacterViewController.swift
+++ iosSchool_HH/Modules/Character/CharacterViewController.swift
@@ -32,7 +32,7 @@ final class CharacterViewController< View: CharacterView>: BaseViewController<Vi
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupBar()
         rootView.backgroundColor = UIColor(named: "charactersBackgroundGray")
         rootView.setView()
         rootView.update(data: CharacterViewData(cells: charactersUrlList.map({
@@ -94,4 +94,18 @@ final class CharacterViewController< View: CharacterView>: BaseViewController<Vi
             }
         }
     }
+    
+    private func setupBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "character-back"),
+            style: .plain,
+            target: self,
+            action: #selector(back)
+        )
+    }
+    @objc private func back() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
+
+
